977c53a431ce1b9323bb5c9ee0f4d19f5436a3b8
gabyslaw <gabyslaw@gmail.com>
2018-02-16 16:50:09 +0100

    Number 4 Assignment

---
+++ Generics/DocumentClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class DocumentClass : IStorable
+    {
+        public void ExportTo()
+        {
+            
+        }
+
+        public void Print()
+        {
+            
+        }
+
+        public void Read()
+        {
+           
+        }
+
+        public void Write()
+        {
+          
+        }
+    }
+
+}
